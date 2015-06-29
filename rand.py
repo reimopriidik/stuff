a639713bfbfbe359336040374b979a91a5c02d5b
Mikk Õun <mikk351@gmail.com>
2015-06-29 15:21:19 +0300

    Update rand.py

--- rand.py
+++ rand.py
@@ -8,10 +8,11 @@ import timeit
 start = timeit.default_timer()
 
 rows = 100000 #Number of rows 
-size=5 #Data length in row
+size = 5 #Data length in row
+fileName = 'rand.csv'
 
 chars=string.ascii_uppercase + string.digits
-with open('rand.csv', 'w') as csvfile:
+with open(fileName, 'w') as csvfile:
 	fieldnames = ['id', 'rand']
 	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 	writer.writeheader()
